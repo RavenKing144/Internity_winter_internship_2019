5b29b2e062f4bf2c8bf93fb98fca2bb73d7cf4e2
Prashi Doval <prashidoval23@gmail.com>
2020-01-03 13:20:21 +0530

    Update heap.cpp

--- team1/task2/Heap Sort/heap.cpp
+++ team1/task2/Heap Sort/heap.cpp
@@ -1,1 +1,23 @@
+template <class Iterator>
+void build_heap (Iterator start, Iterator stop) {
+	unsigned int heapSize = stop - start;
+	for (int i = heapSize / 2; i >= 0; i--)
+		adjust_heap(start, heapSize, i);
+}
+
+template <class Iterator>
+void sort_heap (Iterator start, Iterator stop) {
+	unsigned int lastPosition = stop - start - 1;
+	while (lastPosition > 0) {
+		swap(start[0], start[lastPosition]);
+		adjust_heap(start, lastPosition, 0);
+		lastPosition--;
+		}
+}
+
+template <class Iterator>
+void heap_sort(Iterator start, Iterator stop) {	
+	build_heap (start, stop);
+	sort_heap (start, stop);
+}
 
