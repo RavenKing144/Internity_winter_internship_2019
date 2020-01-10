92ab573cb4bb14a59e054c6e8bc24910163469bb
Sangeeta Jha <43818333+nehasangeetajha@users.noreply.github.com>
2020-01-10 22:30:12 +0530

    Trie.h

---
+++ team1/task3/Sangeeta/Trie.h
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define ALPHABETS 26
+using namespace std;
+class TrieNode
+{
+	string meaning;
+	TrieNode *children[ALPHABETS];
+	friend class Dictionary;
+	public:
+		TrieNode();
+};
+
+class Dictionary
+{
+	TrieNode *root;
+
+	public:
+		Dictionary();
+		bool isEmpty(TrieNode*);
+		TrieNode*removeUtil(TrieNode*,string,int);
+		TrieNode* clearDictionary(TrieNode*);
+		void remove(string);
+		void insert(string,string);
+		void search(string);
+		void autoComplete(TrieNode*,string,vector<string>&);
+		~Dictionary();
+};
