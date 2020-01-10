88c0685e2be76c8fe1699d7124e3e0b4a3db2394
Dhanesh chaudhary <44636841+youngbuck09@users.noreply.github.com>
2020-01-10 21:34:29 +0530

    Add files via upload

---
+++ team 2/project 3/auto.h
@@ -0,0 +1,139 @@
+using namespace std;
+
+#define ALPHABET_SIZE (26)
+
+#define CHAR_TO_INDEX(c) ((int)c - (int)'a')
+vector<string>c;
+int i=0;
+
+struct TrieNode
+{
+    struct TrieNode *children[ALPHABET_SIZE];
+
+    bool isWordEnd;
+};
+
+struct TrieNode *getNode(void)
+{
+    struct TrieNode *pNode = new TrieNode;
+    pNode->isWordEnd = false;
+
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+        pNode->children[i] = NULL;
+
+    return pNode;
+}
+
+void insert(struct TrieNode *root,  const string key)
+{
+    struct TrieNode *pCrawl = root;
+
+    for (int level = 0; level < key.length(); level++)
+    {
+        int index = CHAR_TO_INDEX(key[level]);
+        if (!pCrawl->children[index])
+            pCrawl->children[index] = getNode();
+
+        pCrawl = pCrawl->children[index];
+    }
+
+    pCrawl->isWordEnd = true;
+}
+
+bool search(struct TrieNode *root, const string key)
+{
+    int length = key.length();
+    struct TrieNode *pCrawl = root;
+    for (int level = 0; level < length; level++)
+    {
+        int index = CHAR_TO_INDEX(key[level]);
+
+        if (!pCrawl->children[index])
+            return false;
+
+        pCrawl = pCrawl->children[index];
+    }
+
+    return (pCrawl != NULL && pCrawl->isWordEnd);
+}
+
+bool isLastNode(struct TrieNode* root)
+{
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+        if (root->children[i])
+            return 0;
+    return 1;
+}
+
+void suggestionsRec(struct TrieNode* root, string currPrefix)
+{
+    if (root->isWordEnd)
+    {
+        cout << i+1<<":"<<currPrefix;
+        c.push_back(currPrefix);
+        cout << endl;
+        i++;
+    }
+
+    if (isLastNode(root))
+        return;
+
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+    {
+        if (root->children[i])
+        {
+            // append current character to currPrefix string
+            currPrefix.push_back(97 + i);
+
+            // recur over the rest
+            suggestionsRec(root->children[i], currPrefix);
+        }
+    }
+}
+
+// print suggestions for given query prefix.
+int printAutoSuggestions(TrieNode* root, const string query)
+{
+    struct TrieNode* pCrawl = root;
+
+    // Check if prefix is present and find the
+    // the node (of last level) with last character
+    // of given string.
+    int level;
+    int n = query.length();
+    for (level = 0; level < n; level++)
+    {
+        int index = CHAR_TO_INDEX(query[level]);
+
+        // no string in the Trie has this prefix
+        if (!pCrawl->children[index])
+            return 0;
+
+        pCrawl = pCrawl->children[index];
+    }
+
+    // If prefix is present as a word.
+    bool isWord = (pCrawl->isWordEnd == true);
+
+    // If prefix is last node of tree (has no
+    // children)
+    bool isLast = isLastNode(pCrawl);
+
+    // If prefix is present as a word, but
+    // there is no subtree below the last
+    // matching node.
+    if (isWord && isLast)
+    {
+        cout << query << endl;
+        return -1;
+    }
+
+    // If there are are nodes below last
+    // matching character.
+    if (!isLast)
+    {
+        string prefix = query;
+        suggestionsRec(pCrawl, prefix);
+        return 1;
+    }
+}
