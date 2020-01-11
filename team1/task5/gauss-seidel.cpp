35f95f06f59cd99a1887a90afa0c03fcbfc71e16
Sangeeta Jha <43818333+nehasangeetajha@users.noreply.github.com>
2020-01-11 23:00:40 +0530

    Gauss Seidel Method

---
+++ team1/task5/gauss-seidel.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+#define X3(x1, x2) ((6-(x1)-(x2))/4)
+#define X1(x2, x3) ((12+(x2)-2*(x3))/5)
+#define X2(x1, x3) ((-25-3*(x1)+2*(x3))/8)
+
+int main()
+{
+    double allowed_error;
+    double x1 = 0, x2 = 0, x3 = 0,y1 =0,y2 = 0,y3 =0;
+    int count  =0;
+    cout<<"Enter allowed error value: "<<endl;
+    cin>>allowed_error;
+    printf("\nx1\t\tx2\t\tx3");
+    printf("\n%f\t%f\t%f", x1, x2, x3);
+
+    do
+    {
+        y1 = X1(x2,x3);
+        y2 = X2(y1,x3);
+        y3 = X3(y1,y2);
+
+        if(fabs(y1-x1)<allowed_error && fabs(y2-x2)<allowed_error && fabs(y3-x3)<allowed_error )
+        {
+            cout<<endl<<endl<<"x1 = "<<y1<<endl;
+            cout<<"x2 = "<<y2<<endl;
+            cout<<"x3 = "<<y3<<endl;
+            count=1;
+        }
+        else
+        {
+            x1 = y1;
+            x2 = y2;
+            x3 = y3;
+            printf("\n%f\t%f\t%f", x1, x2, x3);
+        }
+    }while(count!=1);
+    return 0;
+}
