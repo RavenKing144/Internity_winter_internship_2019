ea61e343b1ea98e134d6754b71838d5a9c6d1eff
rustagishubhi <shubhi.rustagi99@gmail.com>
2020-01-16 16:42:17 +0530

    Project 7 RLE

---
+++ team 2/Project 7/rle.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+	ifstream fin;
+	ofstream fout;
+	fin.open("input_file.txt", ios::in);
+	fout.open("compressed_file.txt", ios::out);
+
+	char my_character, last_char;
+	string result = "";
+	int count=1;
+
+	fin.get(my_character);
+	result+=my_character;
+	last_char = my_character;
+
+	while (!fin.eof()) 
+	{
+		fin.get(my_character);
+		// cout << my_character;
+
+		if(last_char == my_character)
+		{
+			count++;
+		}
+		else{
+			result += to_string(count) + string(1, my_character);
+			last_char = my_character;
+			count=1;
+		}
+	}
+	result += to_string(count) + string(1, my_character);
+
+	fout << result;
+	
+	cout << "File Successfully Compressed.\n";
+
+	return 0;
+}
