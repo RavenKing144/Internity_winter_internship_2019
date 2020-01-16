5fbc288bf742545c2bdd9770479a7139868f7bd4
rustagishubhi <shubhi.rustagi99@gmail.com>
2020-01-16 17:10:49 +0530

    Project 7 RLE Decompression

--- team 2/Project 7/rle.cpp
+++ team 2/Project 7/rle.cpp
@@ -14,7 +14,7 @@ int main() {
 	int count=1;
 
 	fin.get(my_character);
-	result+=my_character;
+	// result+=my_character;
 	last_char = my_character;
 
 	while (!fin.eof()) 
@@ -27,12 +27,12 @@ int main() {
 			count++;
 		}
 		else{
-			result += to_string(count) + string(1, my_character);
+			result += to_string(count) + string(1, last_char);
 			last_char = my_character;
 			count=1;
 		}
 	}
-	result += to_string(count) + string(1, my_character);
+	result += to_string(count-1) + string(1, last_char);
 
 	fout << result;
 	
